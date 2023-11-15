8842d4725b52d94355354c6190466b9c7e664fe7
dajiaman <dajiaman@live.com>
2023-11-15 22:32:31 +0800

    update

--- src/business/src/telegram/index.ts
+++ src/business/src/telegram/index.ts
@@ -2,39 +2,43 @@ console.log("telegram preload start");
 import { Subject } from "rxjs";
 
 if (!window["interceptLoader"]) {
-	(window as any)["interceptLoader"] = function (
-		a: any,
-		o: any,
-		e: any,
-		s: any
-	) {
-		const n = (a[e] = { exports: {} });
-		return o[e].call(n.exports, n, n.exports, s), n.exports;
-	};
+  (window as any)["interceptLoader"] = function (
+    a: any,
+    o: any,
+    e: any,
+    s: any
+  ) {
+    const n = (a[e] = { exports: {} });
+    return o[e].call(n.exports, n, n.exports, s), n.exports;
+  };
 }
 
 console.log("window.interceptLoader", (window as any).interceptLoader);
 (window as any).sessionTranslationSetting = {};
 
+// 登录observable
 const authStateObservable$ = new Subject();
-
-authStateObservable$.subscribe((event) => {
-	console.log("event:", event);
+authStateObservable$.subscribe((hasLogin) => {
+  console.log("hasLogin:", hasLogin);
+  if (hasLogin) {
+  } else {
+  }
 });
-setTimeout(() => {
-	authStateObservable$.next(false);
 
-	setTimeout(() => {
-		authStateObservable$.next(true);
-	}, 1000);
+setTimeout(() => {
+  authStateObservable$.next(false);
+
+  setTimeout(() => {
+    authStateObservable$.next(true);
+  }, 1000);
 }, 1000);
 
 class App {
-	doms = new WeakSet();
+  doms = new WeakSet();
 
-	constructor() {}
+  constructor() {}
 
-	handleEvents() {}
+  handleEvents() {}
 }
 
 (window as any)._instance = new App();
